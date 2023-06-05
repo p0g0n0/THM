7406af99a9705fc3e89b2cc007c7e9d47db539fb
p0g0n0 <p0g0n0@proton.me>
2023-06-04 23:35:13 -0300

    Script to answer the first 6 questions

--- THM/Net_Sec_Challenge/hey.py
+++
@@ -1,9 +0,0 @@
-def responsabilidade(x):
-    if x == "pato" or x == "duck" or x == "durck" or x == "pauto" or x == "marina" or x == "mari":
-        return "não é responsável"
-    else:
-        return "é responsável"
-    
-x = input("Diga o nome da espécie:\n-> ")
-
-print(f"{x} {responsabilidade(x)}")
